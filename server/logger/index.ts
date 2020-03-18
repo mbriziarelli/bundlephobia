f23e7fc0aabbfe7346696eda86b640142be8d072
Maxime Briziarelli <maxime.briziarelli@gmail.com>
2020-03-18 23:19:34 +0100

    make logger silent during tests

--- server/logger/index.ts
+++ server/logger/index.ts
@@ -2,6 +2,7 @@ import winston from 'winston'
 
 const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
+  silent: process.env.NODE_ENV === 'test',
 })
 
 export default logger
