b959163a46cd2e9b5d0ae53a15a07bdb55ba8997
Maxime Briziarelli <maxime.briziarelli@gmail.com>
2020-03-18 14:52:58 +0100

    really fix inputfield and autocomplete

--- components/SearchField/index.tsx
+++ components/SearchField/index.tsx
@@ -1,6 +1,5 @@
-import React, { ChangeEvent, FormEvent, ReactElement, useState } from 'react'
+import React, { useState } from 'react'
 import Autocomplete, { RenderInputParams } from '@material-ui/lab/Autocomplete'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import TextField from '@material-ui/core/TextField'
 import { Suggestion } from 'bundlephobia-suggestions-service'
 import usePackageName from '../Hooks/usePackageName'
@@ -12,58 +11,57 @@ const getOptionLabel = (option: Suggestion | null): string =>
 const getOptionSelected = (option1: Suggestion, option2: Suggestion): boolean =>
   option1.name === option2.name && option1.version === option2.version
 
-const SearchField: React.FC = (): ReactElement => {
+const SearchField: React.FC = (): React.ReactElement => {
   const [queryPackageName, setQueryPackageName] = usePackageName()
   const [editedPackageName, setEditedPackageName] = useState(queryPackageName)
   const [isOpen, setIsOpen] = useState(false)
   const suggestions = useSuggestions(editedPackageName)
-  const loading = isOpen && suggestions.length === 0
-
-  const onOpen = (): void => void setIsOpen(true)
-  const onClose = (): void => void setIsOpen(false)
-  const onSubmit = (event: FormEvent): void => {
-    event.preventDefault()
-    setIsOpen(false)
-    setQueryPackageName(editedPackageName)
-  }
-
-  const onInputChange = (
-    event: ChangeEvent<{}>,
-    value: Suggestion | null
-  ): void => void setEditedPackageName(getOptionLabel(value))
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>): void =>
-    void setEditedPackageName(event.target.value)
 
   return (
-    <form noValidate autoComplete="off" onSubmit={onSubmit}>
+    <form
+      noValidate
+      autoComplete="off"
+      onSubmit={(event: React.FormEvent): void => {
+        event.preventDefault()
+        setIsOpen(false)
+        setQueryPackageName(editedPackageName)
+      }}
+    >
       <Autocomplete
         style={{ width: 300 }}
         open={isOpen}
-        onOpen={onOpen}
-        onClose={onClose}
+        onOpen={(): void =>
+          void setIsOpen(editedPackageName.length > 0 && suggestions.length > 0)
+        }
+        onClose={(): void => void setIsOpen(false)}
         getOptionSelected={getOptionSelected}
         getOptionLabel={getOptionLabel}
         options={suggestions}
-        loading={loading}
-        onChange={onInputChange}
-        renderInput={(params: RenderInputParams): ReactElement => (
+        inputValue={editedPackageName}
+        onInputChange={(
+          event: React.ChangeEvent<{}>,
+          value: string,
+          reason: string
+        ): void => {
+          switch (reason) {
+            case 'input':
+            case 'reset':
+              setEditedPackageName(value)
+              break
+            case 'clear':
+              setQueryPackageName('')
+          }
+          setEditedPackageName(value)
+        }}
+        renderInput={(params: RenderInputParams): React.ReactElement => (
           <TextField
             {...params}
             label="Find Package"
             variant="outlined"
-            onChange={onChange}
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <>
-                  {loading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </>
-              ),
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+              setEditedPackageName(event.target.value)
             }}
+            InputProps={{ ...params.InputProps }}
           />
         )}
       />
