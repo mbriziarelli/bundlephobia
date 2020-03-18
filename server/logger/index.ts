fefbe977a295d78d652b19334f2c859cb819e596
Maxime Briziarelli <maxime.briziarelli@gmail.com>
2020-03-18 23:31:50 +0100

    add formats to logger

--- server/logger/index.ts
+++ server/logger/index.ts
@@ -1,6 +1,13 @@
 import winston from 'winston'
 
 const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.label({ label: 'bundlephobia' }),
+    winston.format.timestamp(),
+    winston.format.json({
+      space: process.env.NODE_ENV === 'production' ? 0 : 2,
+    })
+  ),
   transports: [new winston.transports.Console()],
   silent: process.env.NODE_ENV === 'test',
 })
