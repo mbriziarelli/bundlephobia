8002d4f113429cb7d35de516f420319040c903cf
Maxime Briziarelli <maxime.briziarelli@gmail.com>
2020-03-18 09:35:14 +0100

    fix input field and autocomplete

--- components/SearchField/index.tsx
+++ components/SearchField/index.tsx
@@ -1,18 +1,13 @@
-import React, {
-  ChangeEvent,
-  FormEvent,
-  ReactElement,
-  useEffect,
-  useState,
-} from 'react'
-import Autocomplete from '@material-ui/lab/Autocomplete'
-import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+import React, { ChangeEvent, FormEvent, ReactElement, useState } from 'react'
+import Autocomplete, { RenderInputParams } from '@material-ui/lab/Autocomplete'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import TextField from '@material-ui/core/TextField'
 import { Suggestion } from 'bundlephobia-suggestions-service'
 import usePackageName from '../Hooks/usePackageName'
 import useSuggestions from '../Hooks/useSuggestions'
 
-const getOptionLabel = (option: Suggestion): string =>
-  `${option.name}@${option.version}`
+const getOptionLabel = (option: Suggestion | null): string =>
+  option !== null ? `${option.name}@${option.version}` : ''
 
 const getOptionSelected = (option1: Suggestion, option2: Suggestion): boolean =>
   option1.name === option2.name && option1.version === option2.version
@@ -22,66 +17,55 @@ const SearchField: React.FC = (): ReactElement => {
   const [editedPackageName, setEditedPackageName] = useState(queryPackageName)
   const [isOpen, setIsOpen] = useState(false)
   const suggestions = useSuggestions(editedPackageName)
-
-  useEffect(() => {
-    setEditedPackageName(queryPackageName)
-  }, [queryPackageName])
+  const loading = isOpen && suggestions.length === 0
 
   const onOpen = (): void => void setIsOpen(true)
   const onClose = (): void => void setIsOpen(false)
+  const onSubmit = (event: FormEvent): void => {
+    event.preventDefault()
+    setIsOpen(false)
+    setQueryPackageName(editedPackageName)
+  }
 
   const onInputChange = (
     event: ChangeEvent<{}>,
-    value: string,
-    reason: string
-  ): void => {
-    switch (reason) {
-      case 'input':
-        setEditedPackageName(value)
-      case 'clear':
-      case 'reset':
-        setQueryPackageName('')
-    }
-  }
+    value: Suggestion | null
+  ): void => void setEditedPackageName(getOptionLabel(value))
 
   const onChange = (event: ChangeEvent<HTMLInputElement>): void =>
     void setEditedPackageName(event.target.value)
 
-  const renderInput = (params: TextFieldProps): ReactElement => (
-    <TextField
-      {...params}
-      autoFocus
-      label="Find Package"
-      onChange={onChange}
-      value={editedPackageName}
-      variant="outlined"
-    />
-  )
-
   return (
-    <form
-      noValidate
-      autoComplete="off"
-      onSubmit={(event: FormEvent): void => {
-        event.preventDefault()
-        setQueryPackageName(editedPackageName)
-      }}
-    >
+    <form noValidate autoComplete="off" onSubmit={onSubmit}>
       <Autocomplete
-        autoComplete
-        disableOpenOnFocus
-        freeSolo
-        getOptionLabel={getOptionLabel}
-        getOptionSelected={getOptionSelected}
-        includeInputInList
-        noOptionsText="No Suggestions"
-        onClose={onClose}
-        onInputChange={onInputChange}
-        onOpen={onOpen}
+        style={{ width: 300 }}
         open={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        getOptionSelected={getOptionSelected}
+        getOptionLabel={getOptionLabel}
         options={suggestions}
-        renderInput={renderInput}
-        style={{ width: 350 }}
+        loading={loading}
+        onChange={onInputChange}
+        renderInput={(params: RenderInputParams): ReactElement => (
+          <TextField
+            {...params}
+            label="Find Package"
+            variant="outlined"
+            onChange={onChange}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
       />
     </form>
   )
